3e4078b955ee8c12003e14947eb4396178b8b811
Egor <idkhow332@gmail.com>
2026-03-21 17:51:51 +0300

    Fix: подготовка к отправке

---
+++ blogicum/blog/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Category, Location, Post
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'is_published', 'created_at')
+    search_fields = ('title',)
+    list_editable = ('is_published',)
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_published', 'created_at')
+    list_editable = ('is_published',)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'category', 'location', 'is_published', 'created_at')
+    search_fields = ('title', 'text')
+    list_filter = ('is_published', 'category', 'location')
+    list_editable = ('is_published',)
